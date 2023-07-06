48484fb884d6b9cb6f3419a4dde1a05e40034e4f
abhishek101077 <Abhishekgargshu786@gmail.com>
2023-07-06 15:33:55 +0530

    The all programme file

---
+++ Generics/PrintArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class PrintArray<T>
+    {
+        private T[]  inputarray;
+
+        public PrintArray(T[] inputarray)
+        {
+            this.inputarray = inputarray;
+        }
+        public void toprint()
+        {
+            foreach (var element in inputarray)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+
+namespace Generics
+{
+    public class Program
+    {
+      //  public static void toprint<T>(T[] inputArraty)
+      //  {
+        //    foreach (var element in inputArraty)
+         //   {
+          //      Console.WriteLine(element);
+          //  }
+       // }
+        //public static void toprint(double[] inputArraty)
+        //{
+        //    foreach (double element in inputArraty)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //}
+        //public static void toprint(char[] inputArraty)
+        //{
+        //    foreach (char element in inputArraty)
+        //    {
+        //        Console.WriteLine(element);
+        //    }
+        //}
+        public static void Main(string[] args)
+        {
+            int[] inputArray = { 1, 2, 3, 4, };
+            double[] doubleArray = { 1.1, 2.2, 7.7 };
+            char[] charArray = { 'M', 'E', 'S', 'I' };
+
+
+            //  Program.toprint<int>(inputArray);
+            //  Program.toprint<double>(doubleArray);
+            //  Program.toprint<char>(charArray);
+            // PrintArray<int> printArray= new PrintArray<int>(inputArray);
+            // printArray.toprint();
+            //  PrintArray<double> doubleprintArray = new PrintArray<double>(doubleArray);
+            //  doubleprintArray.toprint();
+            //  PrintArray<char> charprintArray = new PrintArray<char>(charArray);
+            //  charprintArray.toprint();
+            find_max.solution(4,5,7);
+        }
+    }
+}
+ 
+    
